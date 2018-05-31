af4efaff9cd7cd84e2f4a2213ebed9b447c49e4c
Ariane van der Steldt <ariane@stack.nl>
2018-05-31 03:52:21 +0100

    Move set_edge() to vertex, where it makes more sense.

--- include/cycle_ptr/detail/vertex.h
+++ include/cycle_ptr/detail/vertex.h
@@ -32,19 +32,132 @@ class vertex
 
   auto reset()
   noexcept
-  -> boost::intrusive_ptr<base_control> {
-    auto ptr = dst_.exchange(nullptr);
-    if (ptr != nullptr) erase_edge(bc_, *ptr);
-    return ptr;
+  -> void {
+    boost::intrusive_ptr<generation> src_gen = bc_->generation_.load();
+
+    // Lock src generation against merges.
+    std::shared_lock<std::shared_mutex> src_merge_lck{ src_gen->merge_mtx_ };
+    while (src_gen != bc_->generation_) {
+      src_merge_lck.unlock();
+      src_gen = bc_->generation_.load();
+      src_merge_lck = std::shared_lock<std::shared_mutex>{ src_gen->merge_mtx_ };
+    }
+
+    // Clear old dst and replace with nullptr.
+    const boost::intrusive_ptr<base_control> old_dst = dst_.exchange(nullptr);
+    if (old_dst != nullptr) {
+      if (old_dst->generation_ != src_gen) {
+        release(dst_);
+      } else {
+        // Because store_refs_ may be a zero reference counter, we can't return
+        // the pointer.
+        const std::uintptr_t refs = old_dst.store_refs_.load(std::memory_order_relaxed);
+        if (get_refs(refs) == 0u && get_color(refs) != color::black)
+          old_dst->gc();
+      }
+    }
   }
 
-  auto reset(const boost::intrusive_ptr<base_control>& new_dst)
+  /**
+   * \brief Assign a new \p new_dst.
+   * \details
+   * Assigns \p new_dst as the pointee of this vertex.
+   *
+   * \param new_dst The destination of the edge. May be null.
+   * \param has_reference If set, \p new_dst has a reference count that will be consumed.
+   * (Ignored for null new_dst.)
+   * \param no_red_promotion If set, caller guarantees no red promotion is required.
+   * Note that this must be set, if has_reference is set.
+   * (Ignored for null new_dst.)
+   */
+  static auto reset(
+      boost::intrusive_ptr<base_control> new_dst,
+      bool has_reference,
+      bool no_red_promotion)
   noexcept
-  -> boost::intrusive_ptr<base_control> {
-    auto ptr = dst_.exchange(new_dst);
-    if (new_dst != nullptr) create_edge(bc_, *new_dst);
-    if (ptr != nullptr) erase_edge(bc_, *ptr);
-    return ptr;
+  -> void {
+    assert(!has_reference || no_red_promotion);
+
+    // Need to special case this, because below we release ````dst_``.
+    if (dst_ == new_dst) {
+      if (new_dst != nullptr && has_reference) new_dst->release();
+      return;
+    }
+
+    // Source generation.
+    // (May be updated below, but must have a lifetime that exceeds either lock.)
+    boost::intrusive_ptr<generation> src_gen = bc_->generation_.load();
+
+    // Declare, but do not acquire, lock for fix_ordering result.
+    std::unique_lock<std::shared_mutex> src_unique_merge_lck;
+
+    // Lock src generation against merges.
+    std::shared_lock<std::shared_mutex> src_merge_lck{ src_gen->merge_mtx_ };
+    while (src_gen != bc_->generation_) {
+      src_merge_lck.unlock();
+      src_gen = bc_->generation_.load();
+      src_merge_lck = std::shared_lock<std::shared_mutex>{ src_gen->merge_mtx_ };
+    }
+
+    // Update ``new_dst`` reference counter for edge.
+    if (new_dst == nullptr) {
+      /* SKIP */
+    } else if (new_dst->generation_ == src_gen) {
+      if (has_reference) new_dst->release(false);
+    } else if (generation::order_invariant(*src_gen, *new_dst->generation_.load())) {
+      if (!has_reference) {
+        if (no_red_promotion)
+          new_dst->acquire_no_red();
+        else
+          new_dst->acquire();
+      }
+    } else {
+      // Reordering of generations needed.
+      src_merge_lck.unlock();
+      src_unique_merge_lck = generation::fix_ordering(*bc_, *new_dst);
+      src_gen = bc_->generation.load(); // Update, since it may have changed.
+      assert(src_unique_merge_lck.owns_lock());
+      assert(src_unique_merge_lck.mutex() == &src_gen.merge_mtx_);
+
+      if (new_dst->generation_ != src_gen) {
+        // Guaranteed by generation::fix_ordering call.
+        assert(generation::order_invariant(src_gen, new_dst->generation_.load()));
+
+        // Acquire reference counter.
+        if (!has_reference) {
+          if (no_red_promotion)
+            new_dst->acquire_no_red();
+          else
+            new_dst->acquire();
+        }
+      } else {
+        // Ensure no reference counter.
+        if (has_reference) new_dst->release(false);
+      }
+    }
+
+    // Assert what's been acquired so far.
+    // If these fail, code above may have corrupted state already.
+    assert(src_merge_lck.owns_lock() || src_unique_merge_lck.owns_lock());
+    assert(!src_merge_lck.owns_lock()
+        || src_merge_lck.mutex() == &src_gen->merge_mtx_);
+    assert(!src_unique_merge_lck.owns_lock()
+        || src_unique_merge_lck.mutex() == &src_gen->merge_mtx_);
+    assert(src_gen == bc_->generation_);
+
+    // Clear old dst and replace with new dst.
+    const boost::intrusive_ptr<base_control> old_dst = dst_.exchange(std::move(new_dst));
+    if (old_dst != nullptr) {
+      if (old_dst->generation_ != src_gen) {
+        release(dst_);
+      } else {
+        // Because store_refs_ may be a zero reference counter, we can't return
+        // the pointer.
+        const std::uintptr_t refs = old_dst.store_refs_.load(std::memory_order_relaxed);
+        if (get_refs(refs) == 0u && get_color(refs) != color::black)
+          old_dst->gc();
+      }
+    }
   }
 
  public:
