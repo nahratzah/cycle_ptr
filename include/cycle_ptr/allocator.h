f60f23773b96996b2f191c7028b60c12b0cc4cdc
Ariane van der Steldt <ariane@stack.nl>
2018-06-04 16:40:23 +0100

    Document allocator.h

--- include/cycle_ptr/allocator.h
+++ include/cycle_ptr/allocator.h
@@ -22,16 +22,23 @@ class cycle_allocator
   template<typename> friend class cycle_allocator;
 
  public:
+  ///\brief When copy-assigning, allocator is not copied over.
   using propagate_on_container_copy_assignment = std::false_type;
+  ///\brief When move-assigning, allocator is not copied over.
   using propagate_on_container_move_assignment = std::false_type;
+  ///\brief When performing swap, allocator is not swapped.
   using propagate_on_container_swap = std::false_type;
+  ///\brief Must check for equality.
   using is_always_equal = std::false_type;
 
+  ///\brief Template for changing controlled type of allocator.
   template<typename T>
   struct rebind {
+    ///\brief The type of this allocator, rebound to type \p T.
     using other = cycle_allocator<typename std::allocator_traits<Nested>::template rebind_alloc<T>>;
   };
 
+  ///\brief Copy constructor for distinct type.
   template<typename Other>
   cycle_allocator(const cycle_allocator<Other>& other)
   noexcept(std::is_nothrow_constructible_v<Nested, const Other&>)
@@ -39,12 +46,18 @@ class cycle_allocator
     control_(other.control_)
   {}
 
+  ///\brief Create allocator, with declared ownership.
+  ///\param base The owner of elements created using this allocator.
+  ///\param args... Arguments to pass to underlying allocator constructor.
   template<typename... Args, typename = std::enable_if_t<std::is_constructible_v<Nested, Args...>>>
   explicit cycle_allocator(const cycle_base& base, Args&&... args)
   : Nested(std::forward<Args>(args)...),
     control_(base.control_)
   {}
 
+  ///\brief Create allocator, with elements having no ownership.
+  ///\param unowned_tag Tag indicating that elements created by this allocator do not have an owning object.
+  ///\param args... Arguments to pass to underlying allocator constructor.
   template<typename... Args, typename = std::enable_if_t<std::is_constructible_v<Nested, Args...>>>
   explicit cycle_allocator([[maybe_unused]] unowned_cycle_t unowned_tag, Args&&... args)
   : Nested(std::forward<Args>(args)...),
@@ -56,6 +69,7 @@ class cycle_allocator
    * \details
    * Publishes the owner control block prior to construction,
    * to allow for members to pick it up automatically.
+   * (After construction, the control block is unpublished.)
    *
    * Forwards to construct as implemented by \p Nested.
    */
@@ -66,6 +80,16 @@ class cycle_allocator
     std::allocator_traits<Nested>::construct(*this, ptr, std::forward<Args>(args)...);
   }
 
+  /**
+   * \brief Fail to create copy of this allocator.
+   * \details
+   * Fails a static assert with a message asking you to please explicitly
+   * specify what ownership relation to use for copied container.
+   *
+   * \note
+   * Ideally, this would also be possible for move construction of a
+   * container, but there's no support for that in the C++ Standard Library.
+   */
   template<typename Dummy = void>
   auto select_on_container_copy_construction()
   -> cycle_allocator {
@@ -74,6 +98,11 @@ class cycle_allocator
     return *this;
   }
 
+  /**
+   * \brief Compare allocators for equality.
+   * \details
+   * Two allocators are equal if they use the same owner.
+   */
   auto operator==(const cycle_allocator& other) const
   noexcept(
       std::allocator_traits<Nested>::is_always_equal::value
@@ -87,6 +116,7 @@ class cycle_allocator
     }
   }
 
+  ///\brief Inequality comparison.
   auto operator!=(const cycle_allocator& other) const
   noexcept(noexcept(*this == other))
   -> bool {
@@ -94,6 +124,7 @@ class cycle_allocator
   }
 
  private:
+  ///\brief Control block for ownership.
   detail::intrusive_ptr<detail::base_control> control_;
 };
 
